d94fddb2d1634e37cc9dceba3b599608a808bc7b
unknown <danubevictoria@gmail.com>
2017-12-18 03:21:15 -0800

    update

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,5 +1,30 @@
 'use strict'
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+	let promises = [];
+
+	for (let i = 0; i < logSources.length; i++) {
+		let source = logSources[i];
+		let promise = source.popAsync();
+		promises.push(promise);
+	}
+
+	Promise.all(promises)
+		.then(function(result) {
+			result.sort(compare);
+		})
+		.catch(function() {
+			return false;
+		});
+
+	for (let j = 0; j < promises.length; j++) {
+		let logEntry = promises[j];
+		printer.print(logEntry);
+	}
+
+	printer.done();
+}
+
+function compare(a, b) {
+	return a.date - b.date;
 }
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,5 +1,24 @@
 'use strict'
 
 module.exports = (logSources, printer) => {
-	throw new Error('Not implemented yet!  That part is up to you!')
+	let sorted = [];
+
+	for (let i = 0; i < logSources.length; i++) {
+		let source = logSources[i];
+		let logEntry = source.pop();
+		sorted.push(logEntry);
+	}
+
+	sorted.sort(compare);
+
+	for (let j = 0; j < sorted.length; j++) {
+		let logEntry = sorted[j];
+		printer.print(logEntry);
+	}
+
+	printer.done();
+}
+
+function compare(a, b) {
+	return a.date - b.date;
 }
